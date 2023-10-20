250c6abc32a136f064c456f031d2072da9cb34dc
HasanH47 <cruweldevil@gmail.com>
2023-10-20 08:04:53 +0700

    update config.php change database

--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'lelang_online');
+define('DB_NAME', 'lelang_php');
 
 // Konfigurasi Sesi
 session_start();
 
 // Lokasi Aplikasi
-define('BASE_URL', 'http://localhost/lelang_online/');
+define('BASE_URL', 'http://localhost/lelang_php/');
